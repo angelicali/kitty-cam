e9b76fd4c3c58968621f34012a9ac3f5cac265bb
angelicali <liyunshuangelica.com>
2024-10-05 16:45:06 -0500

    added logging

--- flask_app.py
+++ flask_app.py
@@ -9,14 +9,14 @@ import threading
 import os
 
 
-OBJECTS_OF_INTEREST = {"cat", "dog"}
+## Model
+model = YOLO("yolov5nu_ncnn_model")
 
-# model = YOLO("yolov8n_ncnn_model")
-model = YOLO("./kitty-cam/yolov5nu_ncnn_model")
-
+## Flask App
 app = flask.Flask('xiaomao-cam', static_folder='static')
-cap = cv2.VideoCapture(0)
 
+# Camera
+cap = cv2.VideoCapture(0)
 
 def cleanup():
     cap.release()
@@ -29,13 +29,16 @@ detected_frames = []
 detection_start_time = None
 last_detected = datetime.now()
 
-def detected_activity(results):
+def log_detected_activity(t, results, logs):
     objects = json.loads(results.to_json())
-    print([obj['name'] for obj in objects], end=' ', flush=True)
-    for obj in objects:
-        if obj['name'] != 'bowl':
-            return True
-    return False
+    object_names = [obj['name'] for obj in objects]
+    if 'bowl' in object_names:
+        object_names.remove('bowl')
+    if len(object_names) == 0:
+        return False
+    timestr = t.strftime('%Y%m%d%H%M%S')
+    logs.append((timestr,','.join(object_names)))
+    return True
 
 def save_frames(frames, detection_start_time):
     timestr = detection_start_time.strftime('%Y%m%d%H%M%S')
@@ -46,10 +49,20 @@ def save_frames(frames, detection_start_time):
         video_writer.write(frame)
     video_writer.release()
 
+def flush_logs(logs):
+    t0 = logs[0][0]
+    t1 = logs[-1][0]
+    filename = f"./logs/{t0}-{t1}.txt"
+    with open(filename, 'w') as f:
+        lines = [f"{t} | {objects}\n" for t, objects in logs]
+        f.writelines(lines)
+
 def run_camera():
     global latest_frame, detected_frames
+    logs = []
     while True:
         ret, frame = cap.read()
+        t = datetime.now()
         if not ret:
             print("reading from camera failed")
             time.sleep(1)
@@ -58,7 +71,7 @@ def run_camera():
         results = model(frame, verbose=False)[0]
         latest_frame = results.plot()
         
-        if detected_activity(results):
+        if log_detected_activity(t, results, logs):
             if len(detected_frames) == 0:
                 detection_start_time = datetime.now()
             detected_frames.append(frame)
@@ -68,6 +81,10 @@ def run_camera():
             if since_last_detection.total_seconds() > 10:
                 save_frames(detected_frames, detection_start_time)
                 detected_frames = []
+
+        if len(logs)>=5000:
+            flush_logs(logs)
+            logs = []
             
 
 def get_video_feed(cap):
